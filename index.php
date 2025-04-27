84d1715a76311d5928ae9de0891cc27a54109d72
Montserrat <71984350+IsadoraMontserrat@users.noreply.github.com>
2025-04-27 20:08:09 -0300

    Update index.php
    O arquivo index.php agora vai inserir registros na tabela dados e também exibir os registros já existentes.

--- index.php
+++ index.php
@@ -9,9 +9,7 @@
 ini_set("display_errors", 1);
 header('Content-Type: text/html; charset=iso-8859-1');
 
-
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+echo 'Versão Atual do PHP: ' . phpversion() . '<br>';
 
 $servername = "54.234.153.24";
 $username = "root";
@@ -19,30 +17,17 @@ $password = "Senha123";
 $database = "meubanco";
 
 // Criar conexão
-
-
 $link = new mysqli($servername, $username, $password, $database);
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+// Verificar a conexão
+if ($link->connect_error) {
+    die("Falha na conexão: " . $link->connect_error);
 }
 
-$valor_rand1 =  rand(1, 999);
+$valor_rand1 = rand(1, 999);
 $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $link->error;
-}
-
-?>
-</body>
-</html>
+// Inserir novo registro
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host)
+          VALUES ('$valor_rand1', '$valor_rand2', '$v
